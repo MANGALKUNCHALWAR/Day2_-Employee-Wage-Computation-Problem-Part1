f8eb70cbad1b0d8c731c3fe766bdd7ca8086360f
John Doe <mangalkunchalwar9999@gmail.com>
2023-03-15 20:23:38 +0530

    Welcome to Employee Wage Computation Program

---
+++ Main.java
@@ -0,0 +1,7 @@
+package day2.employee_wage_computation_problemPart1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
